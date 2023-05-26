26f6d2fb769317715f905c08926977b16d0d0a63
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-26 14:21:47 -0300

    feat: completando req-04

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,30 @@
-# Req 3
+from src.models.dish import Dish
+import csv
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.items = self.read_csv(source_path)
+        self.dishes = self.create_recipes()
+
+    def read_csv(self, path: str):
+        with open(path, "r") as csv_file:
+            menu = list(csv.DictReader(csv_file))
+            return menu
+
+    def create_recipes(self):
+        menu = {}
+
+        for item in self.items:
+            dish_name = item["dish"]
+            dish_price = float(item["price"])
+            ingredient_name = item["ingredient"]
+            recipe_amount = int(item["recipe_amount"])
+
+            dish = menu.setdefault(dish_name, Dish(dish_name, dish_price))
+            ingredient = Ingredient(ingredient_name)
+
+            dish.add_ingredient_dependency(ingredient, recipe_amount)
+
+        return set(menu.values())
