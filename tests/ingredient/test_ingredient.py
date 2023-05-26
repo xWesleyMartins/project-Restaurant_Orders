ae2c05f6e6aa7baff1a7b389fe8e5e4d8f6b5aef
xWesleyMartins <wesley_869@hotmail.com.br>
2023-05-26 10:33:59 -0300

    feat: completando req-01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,22 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_margarina = Ingredient("margarina")
+    ingredient_margarina2 = Ingredient("margarina")
+    ingredient_cafe = Ingredient("cafe")
+    ingredient_farinha = Ingredient("farinha")
+
+    assert hash(ingredient_margarina) == hash(ingredient_margarina2)
+    assert hash(ingredient_cafe) != hash(ingredient_margarina)
+    assert ingredient_margarina.__eq__(ingredient_margarina) is True
+    assert ingredient_margarina.__eq__(ingredient_cafe) is False
+    assert ingredient_margarina.name == "margarina"
+    assert repr(ingredient_margarina) == "Ingredient('margarina')"
+
+    GLUTEN_RESTRICTION = {Restriction.GLUTEN}
+    assert ingredient_farinha.restrictions == GLUTEN_RESTRICTION
